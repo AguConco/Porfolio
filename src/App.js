70cbf4870023ea41560ac72d8801766cb58d6164
AguConco <aguconco9@gmail.com>
2023-03-23 02:22:50 -0300

    inicio porfolio

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from './components/NavBar/NavBar'
+import Home from './pages/Home/Home'
+
+const App = () => {
+  return (
+    <div className="App">
+      <NavBar />
+      <Home />
+    </div>
+  )
+}
+
+export default App;
